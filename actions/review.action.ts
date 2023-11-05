60e1c051128de720b83291b7a1b9eb06e978b9f3
DoiRyoto <r_doi@vislab.cs.tsukuba.ac.jp>
2023-11-06 00:57:45 +0900

    Update review.action.ts

--- actions/review.action.ts
+++ actions/review.action.ts
@@ -3,10 +3,11 @@
 import { collection, getDocs, setDoc, doc, query } from "firebase/firestore"
 import db from "@/lib/firebase/store"
 import { reviewType } from "@/constants"
-import { revalidatePath } from "next/cache"
+import { revalidatePath, unstable_noStore } from "next/cache"
 import { redirect } from "next/navigation"
 
 export async function getAllReviews() {
+    unstable_noStore();
     const col = query(collection(db, "reviews"))
 
     let result: reviewType[] = []
@@ -19,6 +20,7 @@ export async function getAllReviews() {
 }
 
 export async function setReview(reviewData: reviewType) {
+    unstable_noStore();
     await setDoc(doc(db, `reviews/${reviewData.id}`), reviewData)
 
     revalidatePath('/create');
