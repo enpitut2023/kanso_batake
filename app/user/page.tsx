2818add9571a74e69b843c20f2127956b7071ce9
Miura <smiuymiu0official0@gmail.com>
2023-11-15 14:09:58 +0900

    [fix] レビュワーの名前をクリックしたら名前と役職が表示されるようにしました

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -5,7 +5,13 @@ import {miuraData} from '@/constants/'
 const page = () => {
   return (
     <div className="mt-5">
-      {miuraData}
+      <ul>
+        <li>{miuraData.name}</li>
+        {/* <li>{miuraData.affiliation}</li>   */}
+        {/* <li>{miuraData.field}</li>   */}
+        <li>{miuraData.role}</li>  
+        {/* <li>{miuraData.reviews}</li>   */}
+      </ul>
     </div>
   )
 }
