95c64c603746f474f8a43aa78148fc8b1494dc73
DoiRyoto <r_doi@vislab.cs.tsukuba.ac.jp>
2023-11-22 13:43:07 +0900

    update: user actionに追加

--- actions/review.action.ts
+++ actions/review.action.ts
@@ -2,7 +2,7 @@
 
 import { collection, getDocs, setDoc, doc, query, orderBy } from "firebase/firestore"
 import db from "@/lib/firebase/store"
-import { miuraData, reviewType, userType } from "@/constants"
+import { reviewType } from "@/constants"
 import { revalidatePath, unstable_noStore } from "next/cache"
 import { redirect } from "next/navigation"
 
@@ -19,18 +19,30 @@ export async function getAllReviews() {
     return result
 }
 
-export async function setReview(reviewData: reviewType) {
+export async function setReview(userId: string, reviewData: reviewType) {
     unstable_noStore();
-    await setDoc(doc(db, `reviews/${reviewData.id}`), reviewData)
-
+    await Promise.all([setDoc(doc(db, `reviews/${reviewData.id}`), reviewData), setDoc(doc(db, `users/${userId}/reviews/${reviewData.id}`), reviewData)])
+    
     revalidatePath('/create');
     redirect("/")
 }
 
 export async function fetchReviewsByUser(userId: string) {
     unstable_noStore();
-    // const user = await fetchUserData(userId)
-    const user: userType = miuraData
 
-    return user.reviews
+    const col = query(collection(db, `users/${userId}/reviews`), orderBy("id", "desc"))
+
+    let result: reviewType[] = []
+
+    try {
+        const allReviewsSnapshot = await getDocs(col)
+        allReviewsSnapshot.forEach((doc) => {
+            result.push(doc.data() as reviewType)
+        })
+
+        return result
+    } catch (error) {
+        console.log(error)
+        throw new Error("Failed to fetch reviews.")
+    }
 }
