9d7f99186350f3b8b71351833666e0273a76571f
DoiRyoto <r_doi@vislab.cs.tsukuba.ac.jp>
2023-12-13 13:39:55 +0900

    update: 検索バーの追加，レビューフォームの変更

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,15 @@
 import Reviews from "@/components/Reviews"
 import { Suspense } from "react"
 import ReviewPostButton from "@/components/CreateReview"
+import Search from "@/components/TagSearchBar"
 
 export default async function Home() {
   return (
     <div className="flex flex-col gap-10 mt-2">
-      <ReviewPostButton />
+      <div className="flex flex-row gap-3">
+        <Search placeholder="タグを入力"/>
+        <ReviewPostButton />
+      </div>
       <Suspense>
         <Reviews />
       </Suspense>
