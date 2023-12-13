bc3fdcc55ed1b2dc46df4d5827433232a00c839e
takumi-333 <takumi_soccer0514@icloud.com>
2023-12-13 13:38:51 +0900

    [add] tag検索

--- components/Reviews.tsx
+++ components/Reviews.tsx
@@ -1,9 +1,18 @@
-import { getAllReviews } from '@/actions/review.action'
+import { getAllReviews, fetchReviewsByUser } from '@/actions/review.action'
 import React from 'react'
 import Review from './Review'
 
-const Reviews = async () => {
-	const reviewsData = await getAllReviews()
+const Reviews = async ({
+    searchParams,}: {
+  searchParams?: {
+    tag?: string;
+  };
+}
+) => {
+    const tag = searchParams?.tag || '';
+    console.log(tag);
+    const reviewsData = tag == '' ? await getAllReviews() : await
+    fetchReviewsByUser(tag); 
 	
   return (
     <div className="flex flex-col gap-2">
