26f0263aa87d0e3be0a712d4b35a9b320b5b0050
takumi-333 <takumi_soccer0514@icloud.com>
2023-12-15 16:37:59 +0900

    [add] user iconの追加

--- components/Review.tsx
+++ components/Review.tsx
@@ -7,10 +7,12 @@ import {
 	CardTitle,
 } from "@/components/ui/card"
 import Link from 'next/link'
+import Image from 'next/image';
 import { Separator } from './ui/separator'
 import { reviewType } from '@/constants'
 import { SiDoi } from "react-icons/si";
 import { IoIosPaper } from "react-icons/io";
+import icon from "@/public/icon.png"
 
 const Review = (
 	{ reviewData }: { reviewData: reviewType }
@@ -18,27 +20,30 @@ const Review = (
 	return (
 		<Card>
 			<CardHeader>
-				<CardTitle className="truncate leading-normal">
-					{reviewData.paperTitle}
-				</CardTitle>
-				<CardDescription>
-					{reviewData.authors}
-				</CardDescription>
-				<CardDescription>
-					{reviewData.journal_name}. {reviewData.year}. {reviewData.journal_vol}. {reviewData.journal_pages}
-				</CardDescription>
-					<div className="flex flex-row gap-2 py-3">
-						<a href={`https://www.doi.org/${reviewData.doi}`} target='_blank'>
-							<SiDoi size='2rem' />
-						</a>
-						<a href={`${reviewData.link}`} target='_blank'>
-							<IoIosPaper size='2rem' />
-						</a>
-					</div>
+                <CardTitle className="truncate leading-normal">
+                    {reviewData.paperTitle}
+                </CardTitle>
+                <CardDescription>
+                    {reviewData.authors}
+                </CardDescription>
+                <CardDescription>
+                    {reviewData.journal_name}. {reviewData.year}. {reviewData.journal_vol}. {reviewData.journal_pages}
+                </CardDescription>
+                <div className="flex flex-row gap-2 py-3">
+                    <a href={`https://www.doi.org/${reviewData.doi}`} target='_blank'>
+                        <SiDoi size='2rem' />
+                    </a>
+                    <a href={`${reviewData.link}`} target='_blank'>
+                        <IoIosPaper size='2rem' />
+                    </a>
+                </div>
 				<Separator />
 			</CardHeader>
 			<CardContent>
-				Reviewer: <Link href={`/user/${reviewData.createdBy}`}>{reviewData.reviewerName}</Link>
+                <Link href={`/user/${reviewData.createdBy}`} className="flex text-blue-400 hover:text-blue-500 underline flex gap-2">
+                    <Image src={icon} alt="Icon Image" className="rounded" width={24} height={24}/>
+                    {reviewData.reviewerName}
+                </Link>
 			</CardContent>
 			<CardContent className='break-words whitespace-pre-line'>
 				{reviewData.contents}
