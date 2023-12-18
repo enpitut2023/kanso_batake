08b9fa3a466d9fef5f8726f87158b3792ecc174e
DoiRyoto <r_doi@vislab.cs.tsukuba.ac.jp>
2023-12-18 19:11:17 +0900

    update: オンボーディング時にlabsコレクションのユーザーIDを登録する

--- actions/user.action.ts
+++ actions/user.action.ts
@@ -1,27 +1,32 @@
-"use server"
+"use server";
 
 import { userType } from "@/constants";
 import db from "@/lib/firebase/store";
-import { doc, getDoc, setDoc } from "firebase/firestore";
+import { arrayUnion, doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
 
-export async function fetchUser(userId: string){
+export async function fetchUser(userId: string) {
   try {
-    const userData = await getDoc(doc(db, `users/${userId}`))
+    const userData = await getDoc(doc(db, `users/${userId}`));
     if (userData.exists()) {
-      return userData.data() as userType
+      return userData.data() as userType;
     } else {
-      throw new Error("Failed to fetch user.")
+      throw new Error("Failed to fetch user.");
     }
   } catch (error) {
-    console.log(error)
-    throw new Error("Failed to fetch user.")
+    console.log(error);
+    throw new Error("Failed to fetch user.");
   }
 }
 
 export async function setUser(userData: userType) {
   try {
-    await setDoc(doc(db, `users/${userData.id}`), userData)
+    await Promise.all([
+      setDoc(doc(db, `users/${userData.id}`), userData),
+      setDoc(doc(db, `labs/${userData.affiliation}`), {
+        users: arrayUnion(userData.id),
+      }),
+    ]);
   } catch (error) {
-    throw new Error("Failed to set user.")
+    throw new Error("Failed to set user.");
   }
 }
