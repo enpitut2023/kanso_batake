f6f1c5ee38a0e853762b48ae833b9598c93faaf9
DoiRyoto <r_doi@vislab.cs.tsukuba.ac.jp>
2024-01-16 19:30:46 +0900

    update: ハンバーガーメニューの振る舞いを少し修正

--- components/layout/HeaderContents.tsx
+++ components/layout/HeaderContents.tsx
@@ -3,23 +3,26 @@
 import { RxHamburgerMenu } from "react-icons/rx";
 import { HiOutlineExternalLink } from "react-icons/hi";
 import { IoPersonOutline } from "react-icons/io5";
+import { AiOutlineHome } from "react-icons/ai";
+import { IoIosCreate } from "react-icons/io";
 import { ImLab } from "react-icons/im";
 import { Label } from "@/components/ui/label";
 import {
   Sheet,
   SheetClose,
   SheetContent,
-  SheetDescription,
-  SheetFooter,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { userType } from "@/constants";
 import Link from "next/link";
-import { FaHamburger } from "react-icons/fa";
-import Search from "../TagSearchBar";
+
 import { useClerk } from "@clerk/nextjs";
+import { Kaisei_Tokumin } from "next/font/google";
+import clsx from "clsx";
+import Image from "next/image";
+const kaisei = Kaisei_Tokumin({ weight: "400", subsets: ["cyrillic"] });
 
 export function HeaderContents({ user }: { user?: userType }) {
   const { signOut } = useClerk();
@@ -27,19 +30,20 @@ export function HeaderContents({ user }: { user?: userType }) {
   if (!user) {
     return (
       <Sheet>
-        <SheetTrigger asChild>
+        <SheetTrigger asChild className="hover:cursor-pointer">
           <RxHamburgerMenu size={"2rem"} />
         </SheetTrigger>
         <SheetContent>
           <SheetHeader>
-            <SheetTitle>感想畑</SheetTitle>
+            <SheetTitle
+              className={clsx(kaisei.className, "flex flex-row items-center")}
+            >
+              <Image src="/appicon.png" alt="logo" width={30} height={30} />
+              感想畑
+            </SheetTitle>
           </SheetHeader>
           <div className="flex flex-col justify-between h-full pb-4 py-5">
             <div className="flex flex-col gap-3">
-              <div>
-                <Label>レビューの検索</Label>
-                <Search placeholder="タグを入力" />
-              </div>
               <div>
                 <Label>意見・感想はこちら</Label>
                 <a
@@ -64,22 +68,42 @@ export function HeaderContents({ user }: { user?: userType }) {
 
   return (
     <Sheet>
-      <SheetTrigger asChild>
+      <SheetTrigger asChild className="hover:cursor-pointer">
         <RxHamburgerMenu size={"2rem"} />
       </SheetTrigger>
       <SheetContent>
         <SheetHeader>
-          <SheetTitle>感想畑</SheetTitle>
+          <SheetTitle
+            className={clsx(kaisei.className, "flex flex-row items-center")}
+          >
+            <Image src="/appicon.png" alt="logo" width={30} height={30} />
+            感想畑
+          </SheetTitle>
         </SheetHeader>
         <div className="flex flex-col justify-between h-full pb-4 py-5">
           <div className="flex flex-col gap-3">
             <div>
-              <Label>レビューの検索</Label>
-              <Search placeholder="タグを入力" />
+              <Label>投稿する</Label>
+              <SheetClose asChild>
+                <Link
+                  href={`/create`}
+                  className="flex text-lg px-2 py-1 flex-row items-center gap-4 hover:underline"
+                >
+                  <IoIosCreate /> レビューを作成
+                </Link>
+              </SheetClose>
             </div>
             <div>
-              <Label>Explore</Label>
-              <SheetClose className="flex">
+              <Label>閲覧する</Label>
+              <SheetClose asChild>
+                <Link
+                  href={`/`}
+                  className="flex text-lg px-2 py-1 flex-row items-center gap-4 hover:underline"
+                >
+                  <AiOutlineHome /> ホーム
+                </Link>
+              </SheetClose>
+              <SheetClose asChild>
                 <Link
                   href={`/user/${user.id}`}
                   className="flex text-lg px-2 py-1 flex-row items-center gap-4 hover:underline"
@@ -87,7 +111,7 @@ export function HeaderContents({ user }: { user?: userType }) {
                   <IoPersonOutline /> マイページ
                 </Link>
               </SheetClose>
-              <SheetClose className="flex">
+              <SheetClose asChild>
                 <Link
                   href={`/lab/${user.affiliation}`}
                   className="flex text-lg px-2 py-1 flex-row items-center gap-4 hover:underline"
