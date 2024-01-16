94fa9161bec598eb0e5d414d4343c622b6221f66
DoiRyoto <r_doi@vislab.cs.tsukuba.ac.jp>
2024-01-16 15:32:05 +0900

    add: ヘッダーの改善

--- components/layout/HeaderContents.tsx
+++ components/layout/HeaderContents.tsx
@@ -1,50 +1,117 @@
 "use client";
 
+import { RxHamburgerMenu } from "react-icons/rx";
+import { HiOutlineExternalLink } from "react-icons/hi";
+import { IoPersonOutline } from "react-icons/io5";
+import { ImLab } from "react-icons/im";
+import { Label } from "@/components/ui/label";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import { userType } from "@/constants";
-import { useClerk } from "@clerk/nextjs";
-import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import { FaHamburger } from "react-icons/fa";
+import Search from "../TagSearchBar";
+import { useClerk } from "@clerk/nextjs";
 
-const HeaderContents = ({ user }: { user: userType }) => {
+export function HeaderContents({ user }: { user?: userType }) {
   const { signOut } = useClerk();
 
-  return (
-    <div className="container flex flex-row max-w-5xl justify-between p-5 bg-white dark:bg-black">
-      <div className="flex flex-row gap-3">
-        <Link href="/">
-          <Image src="/logo2.png" width={100} height={50} alt="logo" />
-        </Link>
-        <a
-          href="https://forms.gle/PrKwapdJXZniqSZs6"
-          target="_blank"
-          className="flex items-center justify-center"
-        >
-          お問い合わせはこちら
-        </a>
-        <Link href="/lab" className="flex items-center justify-center">
-          研究室一覧
-        </Link>
-      </div>
-      <div className="flex flex-row gap-3">
-        <Link
-          href={`/user/${user.id}`}
-          className="flex items-center justify-center"
-        >
-          マイページ
-        </Link>
-        <Link
-          href={`/lab/${user.affiliation}`}
-          className="flex items-center justify-center"
-        >
-          マイラボ
-        </Link>
-        <button onClick={() => signOut()}>
-          サインアウト
-        </button>
-      </div>
-    </div>
-  );
-};
+  if (!user) {
+    return (
+      <Sheet>
+        <SheetTrigger asChild>
+          <RxHamburgerMenu size={"2rem"} />
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>感想畑</SheetTitle>
+          </SheetHeader>
+          <div className="flex flex-col justify-between h-full pb-4 py-5">
+            <div className="flex flex-col gap-3">
+              <div>
+                <Label>レビューの検索</Label>
+                <Search placeholder="タグを入力" />
+              </div>
+              <div>
+                <Label>意見・感想はこちら</Label>
+                <a
+                  href="https://forms.gle/PrKwapdJXZniqSZs6"
+                  target="_blank"
+                  className="block px-2 py-1"
+                >
+                  <text className="flex flex-row items-center gap-1">
+                    Google Form <HiOutlineExternalLink />
+                  </text>
+                </a>
+              </div>
+            </div>
+            <Link href="/sign-in" className="text-center">
+              サインイン
+            </Link>
+          </div>
+        </SheetContent>
+      </Sheet>
+    );
+  }
 
-export default HeaderContents;
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <RxHamburgerMenu size={"2rem"} />
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>感想畑</SheetTitle>
+        </SheetHeader>
+        <div className="flex flex-col justify-between h-full pb-4 py-5">
+          <div className="flex flex-col gap-3">
+            <div>
+              <Label>レビューの検索</Label>
+              <Search placeholder="タグを入力" />
+            </div>
+            <div>
+              <Label>Explore</Label>
+              <SheetClose className="flex">
+                <Link
+                  href={`/user/${user.id}`}
+                  className="flex text-lg px-2 py-1 flex-row items-center gap-4 hover:underline"
+                >
+                  <IoPersonOutline /> マイページ
+                </Link>
+              </SheetClose>
+              <SheetClose className="flex">
+                <Link
+                  href={`/lab/${user.affiliation}`}
+                  className="flex text-lg px-2 py-1 flex-row items-center gap-4 hover:underline"
+                >
+                  <ImLab /> マイラボ
+                </Link>
+              </SheetClose>
+            </div>
+            <div>
+              <Label>意見・感想はこちら</Label>
+              <a
+                href="https://forms.gle/PrKwapdJXZniqSZs6"
+                target="_blank"
+                className="block px-2 py-1"
+              >
+                <text className="flex flex-row items-center gap-1">
+                  Google Form <HiOutlineExternalLink />
+                </text>
+              </a>
+            </div>
+          </div>
+          <button onClick={() => signOut()}>サインアウト</button>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+}
