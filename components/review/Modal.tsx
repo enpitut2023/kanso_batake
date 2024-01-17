dac2f228366a224af7187acb078d696a900d8b7d
DoiRyoto <r_doi@vislab.cs.tsukuba.ac.jp>
2024-01-17 12:25:46 +0900

    add: modal

--- components/review/Modal.tsx
+++ components/review/Modal.tsx
@@ -1,28 +1,39 @@
-import {
-  Dialog,
-  DialogContent,
-  DialogTrigger,
-} from "@/components/ui/dialog"
-import { AspectRatio } from "../ui/aspect-ratio"
-import Image from "next/image"
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { AspectRatio } from "../ui/aspect-ratio";
+import Image from "next/image";
 
 export function Modal({ imageUrl }: { imageUrl: string }) {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <div className="w-[450px]">
-          <AspectRatio ratio={16 / 9}>
-            <Image src={imageUrl} alt="Image" fill className="rounded-md object-cover" />
-          </AspectRatio>
+        <div className="w-1/5">
+            <Image
+              alt="etc"
+              src={imageUrl}
+              width={1024}
+              height={595}
+              style={{
+                maxWidth: '100%',
+                height: 'auto',
+              }}
+            />
         </div>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <div className="w-[450px]">
-          <AspectRatio ratio={16 / 9}>
-            <Image src={imageUrl} alt="Image" fill className="rounded-md object-cover" />
-          </AspectRatio>
-        </div>
+      <DialogContent className="w-4/5 h-4/5">
+        <AspectRatio ratio={9/16}>
+          <Image
+              alt="etc"
+              src={imageUrl}
+              width={1024}
+              height={595}
+              style={{
+                width: '100%',
+                maxHeight: '100%',
+              }}
+              className="object-contain"
+            />
+        </AspectRatio>
       </DialogContent>
     </Dialog>
-  )
+  );
 }
